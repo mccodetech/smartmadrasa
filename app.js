14669862525ace5496e4814a09949232640e8aa7
mccodetech <chembrasseri1@gmail.com>
2026-08-26 20:13:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2420,9 +2420,9 @@ window.showTab = (tabId) => {
     if (offcanvasInstance) offcanvasInstance.hide();
   }
 
-  if (tabId === 'studentsListTab') window.loadStudentsByClass();
+  if (tabId === 'studentsListTab') loadStudentsByClass();
   if (tabId === 'instAdminTab') window.loadPrincipalsList();
-  if (tabId === 'homeDashboardTab') window.loadLeaderboard();
+  if (tabId === 'homeDashboardTab') loadLeaderboard();
   if (tabId === 'superAdminTab') window.loadSuperAdminRequests();
   if (tabId === 'subjectSettingsTab') window.loadClassSubjectSettings();
 };
